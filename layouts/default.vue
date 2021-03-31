cb267b34724fd2afa4c48f26095bb1f8c3f6e88b
Bruno Gonzales <holabrunogonzales@gmail.com>
2021-03-30 21:35:06 -0500

    Embed tiktok videos

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,60 +2,9 @@
   <div>
     <div class="flex justify-between px-4 py-2">
       <h1>candi<span class="text-red-500">data</span></h1>
-      <a href="https://github.com/brunogonzales/candidata"
-        >Editar esta pagina</a
-      >
+      <img class="object-contain" src="~/assets/search.svg" alt="Busqueda" />
     </div>
     <Nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,133 @@
 <template>
-  <div class="flex flex-col space-y-3 px-4">
-    <h2 class="text-3xl">
-      ¿Por quién votar estas elecciones generales del 11 de Abril del 2021?
-    </h2>
-    <div>
-      <a
-        href="https://www.onpe.gob.pe/modElecciones/elecciones/2021/EEGG/doc/miembro-mesa/como-votar-nacional.pdf"
-        >¿Cómo votar?</a
-      >
-      <a href="https://www.consultamiembrodemesa.eleccionesgenerales2021.pe/#/"
-        >¿Dónde voto?</a
-      >
+  <div class="px-4">
+    <div
+      class="flex items-center justify-center w-16 h-16 border-solid border-4 border-red-500 rounded-full inline-block"
+    >
+      <img
+        class="w-8 object-contain"
+        src="~/assets/juntos-por-el-peru.png"
+        alt=""
+      />
     </div>
-    <div class="flex space-x-2">
-      <p class="text-red-500">Presidentes</p>
-      <p>Congresistas</p>
-      <p>Partido Andino</p>
-    </div>
-    <div>
-      <a
-        href="https://es.wikipedia.org/wiki/Juntos_por_el_Per%C3%BA"
-        class="text-3xl font-bold"
-        >Juntos por el Perú</a
-      >
-      <div class="flex">
-        <img
-          class="object-contain "
-          src="~/assets/veronika_mendoza.png"
-          alt="Juntos por el Peru y Veronika Mendoza"
-        />
-        <div class="flex flex-col">
-          <a href="https://twitter.com/Vero_Mendoza_F">Verónika Mendoza</a>
-          <p class="text-xs text-gray-400">Candidata presidencial</p>
-          <a
-            href="https://apisije-e.jne.gob.pe/TRAMITE/ESCRITO/1587/ARCHIVO/FIRMADO/5262.PDF"
-            >Plan de gobierno</a
-          >
-          <a href="https://www.instagram.com/p/CM4t88TJw_V/"
-            >Propuestas notables</a
-          >
-        </div>
+    <a
+      target="_blank"
+      href="https://es.wikipedia.org/wiki/Juntos_por_el_Per%C3%BA"
+      class="text-3xl font-bold"
+      >Juntos por el Perú</a
+    >
+    <div class="flex">
+      <img
+        class="object-contain"
+        src="~/assets/veronika-mendoza.png"
+        alt="Veronika Mendoza"
+      />
+      <div class="flex flex-col">
+        <a target="_blank" href="https://twitter.com/Vero_Mendoza_F"
+          >Verónika Mendoza</a
+        >
+        <p class="text-xs text-gray-400">Candidata presidencial</p>
+        <a
+          target="_blank"
+          href="https://apisije-e.jne.gob.pe/TRAMITE/ESCRITO/1587/ARCHIVO/FIRMADO/5262.PDF"
+          >Plan de gobierno</a
+        >
+        <a target="_blank" href="https://www.instagram.com/p/CM4t88TJw_V/"
+          >Propuestas notables</a
+        >
       </div>
     </div>
+    <section>
+      <blockquote
+        class="tiktok-embed"
+        cite="https://www.tiktok.com/@veromendozaperu/video/6945499877338041605"
+        data-video-id="6945499877338041605"
+        style="max-width: 605px; min-width: 325px"
+      >
+        <section>
+          <a
+            target="_blank"
+            title="@veromendozaperu"
+            href="https://www.tiktok.com/@veromendozaperu"
+            >@veromendozaperu</a
+          >
+          <p>
+            Quiero ser presidente para
+            <a
+              title="foryou"
+              target="_blank"
+              href="https://www.tiktok.com/tag/foryou"
+              >##foryou</a
+            >
+            <a
+              title="debate2021"
+              target="_blank"
+              href="https://www.tiktok.com/tag/debate2021"
+              >##debate2021</a
+            >
+            <a
+              title="veronikamendoza"
+              target="_blank"
+              href="https://www.tiktok.com/tag/veronikamendoza"
+              >##veronikamendoza</a
+            >
+          </p>
+          <a
+            target="_blank"
+            title="♬ Steven Universe - L.Dre"
+            href="https://www.tiktok.com/music/Steven-Universe-6795585271782967298"
+            >♬ Steven Universe - L.Dre</a
+          >
+        </section>
+      </blockquote>
+      <script async src="https://www.tiktok.com/embed.js"></script>
+      <blockquote
+        class="tiktok-embed"
+        cite="https://www.tiktok.com/@veromendozaperu/video/6945275982702038277"
+        data-video-id="6945275982702038277"
+        style="max-width: 605px; min-width: 325px"
+      >
+        <section>
+          <a
+            target="_blank"
+            title="@veromendozaperu"
+            href="https://www.tiktok.com/@veromendozaperu"
+            >@veromendozaperu</a
+          >
+          <p>
+            Ataque a @larutadekeiko
+            <a
+              title="veronikamendoza"
+              target="_blank"
+              href="https://www.tiktok.com/tag/veronikamendoza"
+              >##veronikamendoza</a
+            >
+            <a
+              title="keikofujimori"
+              target="_blank"
+              href="https://www.tiktok.com/tag/keikofujimori"
+              >##keikofujimori</a
+            >
+            <a
+              title="foryou"
+              target="_blank"
+              href="https://www.tiktok.com/tag/foryou"
+              >##foryou</a
+            >
+          </p>
+          <a
+            target="_blank"
+            title="♬ SugarCrash! - ElyOtto"
+            href="https://www.tiktok.com/music/SugarCrash-6920125567752734722"
+            >♬ SugarCrash! - ElyOtto</a
+          >
+        </section>
+      </blockquote>
+      <script async src="https://www.tiktok.com/embed.js"></script>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  async mounted() {
-    const data = await this.$strapi.graphql({
-      query: `
-    query {
-      organizations {
-        name
-      }
-    }
-  `
-    });
-
-    console.log(data);
-  }
-};
+export default {};
 </script>
 
-<style></style>
